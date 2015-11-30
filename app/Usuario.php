f3e92be9f871a20b4a4833e05643eae2158c797f
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-11-30 17:43:29 -0300

    Declara atributos que pueden ser llenado con asignacion masiva

--- app/Usuario.php
+++ app/Usuario.php
@@ -7,4 +7,12 @@ class Usuario extends Model {
     protected $table = 'usuarios';
     protected $primaryKey = 'id';
 
+    protected $fillable = [
+
+        'nombre',
+        'apellido_p',
+        'apellido_m',
+        'email'
+    ];
+
 }
