b6c7d18ecea43a317bdfd4a9f76db008ee918cc7
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-04 19:11:42 -0300

    Agrega URLs para avatares de usuarios

--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -11,13 +11,30 @@ class UsuariosSeeder extends Seeder
 
         $faker = \Faker\Factory::create();
 
+        \DB::table('usuarios')->truncate();
+
+        $avatar = [
+            'https://avatars.githubusercontent.com/u/5294167',
+            'https://avatars.githubusercontent.com/u/5294169',
+            'https://avatars.githubusercontent.com/u/5294172',
+            'https://avatars.githubusercontent.com/u/5294173',
+            'https://avatars.githubusercontent.com/u/5294174',
+            'https://avatars.githubusercontent.com/u/5294175',
+            'https://avatars.githubusercontent.com/u/5294176',
+            'https://avatars.githubusercontent.com/u/5294177',
+            'https://avatars.githubusercontent.com/u/5294178',
+            'https://avatars.githubusercontent.com/u/5294179',
+            'https://avatars.githubusercontent.com/u/5294180',
+        ];
+
         for ($i = 0; $i < 100; $i++) {
 
             Usuario::create([
                 'nombre' => $faker->name,
                 'apellido_p' => $faker->lastName,
                 'apellido_m' => $faker->lastName,
-                'email' => $faker->email
+                'email' => $faker->email,
+                'url_avatar'=>$faker->randomElement($avatar)
             ]);
         }
 
