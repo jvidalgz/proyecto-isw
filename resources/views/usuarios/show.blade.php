a07acccc57c652a1348aab27d139f393f99035b9
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-04 19:13:17 -0300

    Actializa vista de un usuario. Agrega clases bootstrap

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -1,4 +1,33 @@
-{!! $usuario->nombre !!}
-{!! $usuario->apellido_p !!}
-{!! $usuario->apellido_m !!}
-{!! $usuario->email !!}
+@extends('master-layout')
+
+@section('contenido')
+    <div class="container-fluid well span6">
+    <div class="row-fluid">
+        <div class="span2" >
+            <img src="{!! $usuario->url_avatar !!}" width="100" alt="avatar" class="img-thumbnail">
+        </div>
+
+        <div class="span8">
+            <h3>Usuario: {!! $usuario->nombre !!}</h3>
+            <h6>Apellido Paterno: {!! $usuario->apellido_p !!}</h6>
+            <h6>Aepllido Materno: {!! $usuario->apellido_m!!}</h6>
+            <h6>Email: {!! $usuario->email !!}</h6>
+
+            <h6><a href="#">More... </a></h6>
+        </div>
+
+        <div class="span2">
+            <div class="btn-group">
+                <a class="btn dropdown-toggle btn-info" data-toggle="dropdown" href="#">
+                    Acción
+                    <span class="icon-cog icon-white"></span><span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="{!! URL::to('usuarios/'.$usuario->id.'/edit')!!}"><span class="glyphicon glyphicon-edit"></span> Editar</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-trash"></span> Eliminar</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+@stop('contenido')
