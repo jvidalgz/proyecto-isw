24f99d6e9bb7933f0425e02c355383f757fd73c1
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 11:08:21 -0300

    Actualiza etiquetas html y agrega comentarios en los div de cierre

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -6,27 +6,31 @@
         <li class="active">{!! $usuario->nombre !!}</li>
     </ol>
 
-        <div class="span8">
-            <h3>Usuario: {!! $usuario->nombre !!}</h3>
-            <h6>Apellido Paterno: {!! $usuario->apellido_p !!}</h6>
-            <h6>Aepllido Materno: {!! $usuario->apellido_m!!}</h6>
-            <h6>Email: {!! $usuario->email !!}</h6>
+    <div class="well span6">
+        <div class="row-fluid">
+            <div class="span2" >
+                <img src="{!! $usuario->url_avatar !!}" width="100" alt="avatar" class="img-thumbnail">
+            </div> <!-- /span2 -->
 
-            <h6><a href="#">More... </a></h6>
-        </div>
+            <div class="span8">
+                <h3>Usuario: {!! $usuario->nombre !!}</h3>
+                <h6>Apellido Paterno: {!! $usuario->apellido_p !!}</h6>
+                <h6>Aepllido Materno: {!! $usuario->apellido_m!!}</h6>
+                <h6>Email: {!! $usuario->email !!}</h6>
+            </div> <!-- /span8 -->
 
-        <div class="span2">
-            <div class="btn-group">
-                <a class="btn dropdown-toggle btn-info" data-toggle="dropdown" href="#">
-                    Acción
-                    <span class="icon-cog icon-white"></span><span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="{!! URL::to('usuarios/'.$usuario->id.'/edit')!!}"><span class="glyphicon glyphicon-edit"></span> Editar</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-trash"></span> Eliminar</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    </div>
+            <div class="span2">
+                <div class="btn-group">
+                    <a class="btn dropdown-toggle btn-info" data-toggle="dropdown" href="#">
+                        Acción
+                        <span class="icon-cog icon-white"></span><span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{!! URL::to('usuarios/'.$usuario->id.'/edit')!!}"><span class="glyphicon glyphicon-edit"></span> Editar</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-trash"></span> Eliminar</a></li>
+                    </ul>
+                </div> <!-- /btn-group -->
+            </div> <!-- /span2 -->
+        </div> <!-- /row-fluid -->
+    </div> <!-- /well span6 -->
 @stop('contenido')
