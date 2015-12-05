9021ebed4ffa1a67460840c494ef448137f09656
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 10:51:05 -0300

    Hereda de master-layout. Agrega clases bootstrap y etiqueta table para mostrar todos los resultados dentro de una tabla

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -1,5 +1,29 @@
-@foreach($usuarios as $usuario)
-    {!! $usuario->nombre !!}
-    {!! $usuario->apellido_p !!}
-    {!! $usuario->apellido_m !!}
-@endforeach
+@extends('master-layout')
+    @section('contenido')
+        <table class="table table-bordered">
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Email</th>
+            <th>Acciones</th>
+                <tbody>
+                @foreach($usuarios as $usuario)
+                    <tr>
+                        <td>{!! $usuario->nombre !!}</td>
+                        <td>{!! $usuario->apellido_p !!}</td>
+                        <td>{!! $usuario->apellido_m !!}</td>
+                        <td>{!! $usuario->email !!}</td>
+                        <td>
+                            <a class="btn btn-xs btn-primary" href="{!! URL::to('usuarios/'.$usuario->id) !!}">Ver</a>
+                            <a class="btn btn-xs btn-success" href="{!! URL::to('usuarios/'.$usuario->id.'/edit') !!}">Editar</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+        </table>
+        <!-- Paginacion -->
+       <div class="text-center"> {!! $usuarios->render() !!}</div>
+    @stop('contenido')
+
+
+
