149da60d74450c1b8a698ceb893859e6f3b296e4
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 14:41:33 -0300

    Agraga barra principal para toda la aplicacion (sitio)

--- resources/views/master-layout.blade.php
+++ resources/views/master-layout.blade.php
@@ -11,6 +11,41 @@
         {!! Html::style('css/bootstrap.min.css') !!}
     </head>
     <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand">
+                    <img alt="Brand" src="{!! URL::to('img/logo-ubb.png') !!}">
+                </a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{!! URL::to('usuarios') !!}">Proyecto ISW</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link Ejemplo</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown ejemplo <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Acción</a></li>
+                            <li><a href="#">Otra Acción</a></li>
+                            <li><a href="#">Algo más acá</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Link separado</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
         <div class="container">
             @yield('contenido')
