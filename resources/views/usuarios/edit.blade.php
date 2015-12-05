aa77089774cae3e6b4a05cdc30c7d60f9d655e4a
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-05 11:31:02 -0300

    Agrega estilo al formulario

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -1,20 +1,31 @@
 @extends('master-layout')
     @section('contenido')
-        {!! Form::model($usuario,['route'=>['usuarios.update',$usuario],'method'=> 'PUT']) !!}
 
-            {!! Form::label('nombre', 'Nombre') !!}
-            {!! Form::text('nombre', null,array('placeholder' => 'Lalo','autocomplete'=>'off')) !!}
+        {!! Form::model($usuario,['route'=>['usuarios.update',$usuario],'method'=> 'PUT','class' =>'form-horizontal']) !!}
 
-            {!! Form::label('apellido_p', 'Apellido Paterno') !!}
-            {!! Form::text('apellido_p', null) !!}
+           <div class="form-group">
+               {!! Form::label('nombre', 'Nombre', ['class'=>'control-label']) !!}
+               {!! Form::text('nombre', null, ['class'=>'form-control']) !!}
+           </div>
 
-            {!! Form::label('apellido_m', 'Apellido Materno') !!}
-            {!! Form::text('apellido_m', null) !!}
+            <div class="form-group">
+                {!! Form::label('apellido_p', 'Apellido Paterno', ['class'=>'control-label']) !!}
+                {!! Form::text('apellido_p', null, ['class'=>'form-control']) !!}
+            </div>
 
-            {!! Form::label('email', 'Email') !!}
-            {!! Form::text('email', null) !!}
+            <div class="form-group">
+                {!! Form::label('apellido_m', 'Apellido Materno', ['class'=>'control-label']) !!}
+                {!! Form::text('apellido_m', null, ['class'=>'form-control']) !!}
+            </div>
 
-            {!! Form::submit('Guardar') !!}
+            <div class="form-group">
+                {!! Form::label('email', 'Email', ['class'=>'control-label']) !!}
+                {!! Form::text('email', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Guardar',['class'=>'btn btn-success']) !!}
+            </div>
 
         {!! Form::close() !!}
     @stop
