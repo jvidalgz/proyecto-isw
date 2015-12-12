c093a8ed10db452f53c865e461bca5a280b8e197
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-12 19:26:57 -0300

    Actualiza vista para el registro de nuevos usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,14 @@
-@extends('app')
+@extends('master-layout')
 
-@section('content')
-<div class="container-fluid">
+@section('contenido')
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
+				<div class="panel-heading">Registro nuevo usuario</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Whoops!</strong> Hay errores en en registro<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -22,37 +21,49 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
+							<label class="col-md-4 control-label">Nombre</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								<input type="text" class="form-control" name="nombre" value="{{ old('nombre') }}" autocomplete="off">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Apellido Materno</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="apellido_p" value="{{ old('apellido_p') }}" autocomplete="off">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-4 control-label">Apellido Materno</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="apellido_m" value="{{ old('apellido_m') }}" autocomplete="off">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">E-Mail</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="off">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label class="col-md-4 control-label">Clave</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								<input type="password" class="form-control" name="password" autocomplete="off">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
+							<label class="col-md-4 control-label">Confirmar Clave</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+								<input type="password" class="form-control" name="password_confirmation" autocomplete="off">
 							</div>
 						</div>
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-primary">
-									Register
+									Registrar
 								</button>
 							</div>
 						</div>
@@ -61,5 +72,5 @@
 			</div>
 		</div>
 	</div>
-</div>
+
 @endsection
