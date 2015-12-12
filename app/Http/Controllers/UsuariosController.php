6aedc2f27ae44e67e8c64fe7a11e4afb79790d55
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-12 20:00:04 -0300

    Se especifica en la funcion constructor de este Controlador que se ejecutara el Middleware 'auth' ANTES de ejecutar alguns funcion de este Controlador

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 
     /**
      * Muestra todos los usuarios
