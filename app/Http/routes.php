4f14310b8ca4fbada529677d4203ae806bb758ca
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-12 18:54:10 -0300

    Agrega rutas para manejar el registro y autenticacion de usuarios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,11 @@ Route::get('/', 'WelcomeController@index');
 Route::get('/deal-with-it', 'WelcomeController@deal');
 Route::get('/deal-with-it-extreme', 'WelcomeController@dealExtreme');
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+// Rutas para registro y autenticación de usuarios
+Route::get('auth/register', 'Auth\AuthController@getRegister'); // Muestra el formulario para el registro de un usuario
+Route::post('auth/register', 'Auth\AuthController@postRegister'); // Recibe los datos del formulario de registro de usuarios
+Route::get('auth/login', 'Auth\AuthController@getLogin'); // Muestra el formulario para iniciar sesión
+Route::post('auth/login', 'Auth\AuthController@postLogin'); // Recibe los datos para iniciar sesión
+Route::get('auth/logout', 'Auth\AuthController@getLogout'); // Ruta para cerrar sesión de usuario
 
 Route::resource('usuarios','UsuariosController');
