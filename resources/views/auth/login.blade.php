769915ed6c5852a2c3dc572066c2baf239a87b30
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-12 19:26:39 -0300

    Actualiza vista para inicio de sesion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,14 @@
-@extends('app')
+@extends('master-layout')
 
-@section('content')
-<div class="container-fluid">
+@section('contenido')
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
+				<div class="panel-heading">Inicio de sesión</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Whoops!</strong> Hay problemas con los datos que se ingresaron<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -22,34 +21,23 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">E-Mail</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								<input type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="off">
 							</div>
 						</div>
 
 						<div class="form-group">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								<input type="password" class="form-control" name="password" autocomplete="off">
 							</div>
 						</div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+								<button type="submit" class="btn btn-primary">Acceder</button>
 							</div>
 						</div>
 					</form>
@@ -57,5 +45,4 @@
 			</div>
 		</div>
 	</div>
-</div>
 @endsection
