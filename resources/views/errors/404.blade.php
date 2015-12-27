779af9a84fd1c521dc2d0122332ce5e44c3640cd
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2015-12-27 16:39:59 -0300

    Crea vista para ser mostrar cuando existan errores 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('master-layout')
+
+
+
+
+
+@section('contenido')
+    <h1>Error 404 - Página no encontrada</h1>
+
+    <div class="panel panel-danger">
+        <div class="panel-heading">
+            <h3 class="panel-title">Atención!</h3>
+        </div>
+        <div class="panel-body">
+            La página que Usted solicitó no se encuentra
+        </div>
+    </div>
+
+
+
+@stop
